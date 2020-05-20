9be086a4c0d65032b2cd92d45478b8eb0c99eea5
Wojti <wdziuba@student.agh.edu.pl>
2020-05-20 23:50:42 +0200

    Init

---
+++ Assets/Scripts/Machine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
+using UnityEngine;
+
+
+public class Machine : MonoBehaviour
+{
+    [SerializeField] private float _processingTime = 4000;
+    [SerializeField] private bool _isWorking = false;
+    [SerializeField] private Vector2 queueBegginingOffset = new Vector2(0, 0.25f);
+    [SerializeField] private Vector2 nextQueuedParticleOffset = new Vector2(0, 0.25f);
+
+    private Timer workTimer;
+    
+    public Queue<GameObject> pastaQueue = new Queue<GameObject>();
+    public PastaParticleControler currentryWorkedOnPastaParticle;
+
+    public float ProcessingTime
+    {
+        get
+        {
+            return _processingTime;
+        }
+        set
+        {
+            _processingTime = value * 1000;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        workTimer = new Timer(_processingTime);
+        workTimer.Elapsed += SendToNextMachine;
+        workTimer.Enabled = true;
+        workTimer.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isWorking == false && pastaQueue.Count > 0)
+        {
+            DoWork();
+        }
+    }
+
+
+    private void DoWork()
+    {
+        _isWorking = true;
+        currentryWorkedOnPastaParticle = GetFromPastaQueue().GetComponent<PastaParticleControler>();
+
+        currentryWorkedOnPastaParticle.transform.position = this.transform.position;
+
+        workTimer.Start();
+    }
+
+    private void SendToNextMachine(object source, ElapsedEventArgs e)
+    {
+        workTimer.Stop();
+        currentryWorkedOnPastaParticle.LetsFuckingGoBaby();
+        _isWorking = false;
+    }
+
+    private GameObject GetFromPastaQueue()
+    {
+        GameObject temp = pastaQueue.Dequeue();
+        ArrangeQueue();
+        return temp;
+    }
+
+    public void AddToPastaQueue(GameObject pastaParticle)
+    {
+        pastaQueue.Enqueue(pastaParticle);
+        ArrangeQueue();
+    }
+
+    void ArrangeQueue()
+    {
+        Vector2 machinePosition = this.transform.position;
+        machinePosition += queueBegginingOffset;
+
+        Vector2 offset = new Vector2();
+
+        foreach(GameObject pastaParticle in pastaQueue)
+        {
+            pastaParticle.transform.position = machinePosition + offset;
+            offset += nextQueuedParticleOffset;
+        }
+
+    }
+}
+
+
+
---
+++ Assets/Scripts/PastaParticleControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PastaParticleControler : MonoBehaviour
+{
+    [SerializeField] private float speed = 2;
+    [SerializeField] private int currentTargetPointer = 1;
+    [SerializeField] private bool _movementToggle = true;
+
+    public List<GameObject> AssemblyLineMachinesRoute = new List<GameObject>();
+
+    void Start()
+    {
+        //this.transform.position = AssemblyLineMachinesRoute[0].transform.position;
+        currentTargetPointer = 0;
+    }
+
+    void Update()
+    {
+        if (_movementToggle) GoToMachine();
+    }
+
+    public void LetsFuckingGoBaby()
+    {
+        _movementToggle = true;
+    }
+
+    public void LetsFuckingStopBaby()
+    {
+        _movementToggle = false;
+    }
+
+    private void GoToMachine()
+    {
+        if(Vector2.Distance(this.transform.position, AssemblyLineMachinesRoute[currentTargetPointer].transform.position) > 0.01f)
+        {
+            float step = speed * Time.deltaTime;
+            this.transform.position = Vector2.MoveTowards(this.transform.position, AssemblyLineMachinesRoute[currentTargetPointer].transform.position, step);
+        }
+        else
+        {
+            //TODO jakieś pracki na maszynie czekanie pierdoły
+            //TODO dodać Brrr 
+
+            AssemblyLineMachinesRoute[currentTargetPointer].GetComponent<Machine>().AddToPastaQueue(this.gameObject);
+            currentTargetPointer++;
+            _movementToggle = false;
+        }
+    }
+
+}
