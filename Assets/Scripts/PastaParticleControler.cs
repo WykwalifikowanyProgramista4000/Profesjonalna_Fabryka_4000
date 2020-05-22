ad18e14ceb2b46128b37f2d7db8985e8b6b8b0b8
Mikolaj <mikolajd4agh@gmail.com>
2020-05-22 21:11:46 +0200

    uszkodzenie particla solo

--- Assets/Scripts/PastaParticleControler.cs
+++ Assets/Scripts/PastaParticleControler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class PastaParticleControler : MonoBehaviour
 {
     [SerializeField] private float speed = 2;
     [SerializeField] private int currentTargetPointer = 1;
+    [SerializeField] private int flawThreshold = 10; //ilość uszkodzonych w procentach
+    [SerializeField] private bool isBroken = false;
 
     public bool movementToggle = true;
     public List<GameObject> AssemblyLineMachinesRoute = new List<GameObject>();
@@ -13,21 +16,26 @@ public class PastaParticleControler : MonoBehaviour
     void Start()
     {
         currentTargetPointer = 0;
+        System.Random random = new System.Random();
+        int particleBrokenRanodmizer = random.Next(0, 100);
+        if (particleBrokenRanodmizer < flawThreshold) isBroken = true;
     }
 
     void Update()
     {
         if (movementToggle) GoToMachine();
+        if (isBroken) GetComponent<SpriteRenderer>().color = Color.red; //wadliwe particle przedstawione na czerwono
+
     }
 
     private void GoToMachine()
     {
-        if(Vector2.Distance(this.transform.position, AssemblyLineMachinesRoute[currentTargetPointer].transform.position) > 0.01f)
+        if (Vector2.Distance(this.transform.position, AssemblyLineMachinesRoute[currentTargetPointer].transform.position) > 0.01f)
         {
             float step = speed * Time.deltaTime;
             this.transform.position = Vector2.MoveTowards(this.transform.position, AssemblyLineMachinesRoute[currentTargetPointer].transform.position, step);
         }
-        else if (AssemblyLineMachinesRoute[currentTargetPointer].CompareTag("Destroyer"))
+        else if(AssemblyLineMachinesRoute[currentTargetPointer].CompareTag("Destroyer"))
         {
             AssemblyLineMachinesRoute[currentTargetPointer].GetComponent<Destroyer>().DestroyPastaParticle(this.gameObject);
         }
