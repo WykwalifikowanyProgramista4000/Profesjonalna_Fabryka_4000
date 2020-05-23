74e9e43daa77f7e4b6b8bb9c75f43129469c3e5d
Jacek Wojtowicz <jacekwojtowicz98@gmail.com>
2020-05-23 22:24:43 +0200

    Przetwarzanie makaronu przez maszyne jako kolejka (do poprawienia

--- Assets/Scripts/Machine.cs
+++ Assets/Scripts/Machine.cs
@@ -11,16 +11,20 @@ public class Machine : MonoBehaviour
 {
     #region Properties
     [SerializeField] private float _processingTime = 4000;
+    [SerializeField] private int _throughput = 3;
     [SerializeField] private bool _isWorking = false;
     [SerializeField] private Vector2 queueBegginingOffset = new Vector2(0, 0.25f);
     [SerializeField] private Vector2 nextQueuedParticleOffset = new Vector2(0, 0.25f);
+    [SerializeField] private Vector2 machinequeueBegginingOffset = new Vector2(-0.1f, 0);
+    [SerializeField] private Vector2 machinenextQueuedParticleOffset = new Vector2(0.05f, 0);
 
     private Timer workTimer;
     private Brrr brrr;
     
     public Queue<GameObject> pastaQueue = new Queue<GameObject>();
-    public PastaParticleControler currentryWorkedOnPastaParticle1;
-    public PastaParticleControler currentryWorkedOnPastaParticle2;
+    public Queue<GameObject> pasta_in_machine_Queue = new Queue<GameObject>();
+    public PastaParticleControler currentryWorkedOnPastaParticle;
+    //public PastaParticleControler currentryWorkedOnPastaParticle2;
 
     #endregion
 
@@ -53,7 +57,7 @@ public class Machine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_isWorking == false && pastaQueue.Count > 1)
+        if (_isWorking == false && pastaQueue.Count > _throughput - 1)
         {
             DoWork();
         }
@@ -65,20 +69,36 @@ public class Machine : MonoBehaviour
     private void DoWork()
     {
         _isWorking = true;
-        currentryWorkedOnPastaParticle1 = GetFromPastaQueue().GetComponent<PastaParticleControler>();
-        currentryWorkedOnPastaParticle2 = GetFromPastaQueue().GetComponent<PastaParticleControler>();
+        for(int i=0;i< _throughput;i++)
+        {
+            currentryWorkedOnPastaParticle = GetFromPastaQueue().GetComponent<PastaParticleControler>();
+            AddToPastaInMachineQueue(currentryWorkedOnPastaParticle.gameObject);
 
-        currentryWorkedOnPastaParticle1.transform.position = this.transform.position + new Vector3(0.1f,0,0);
-        currentryWorkedOnPastaParticle2.transform.position = this.transform.position + new Vector3(-0.1f, 0, 0); ;
+            //currentryWorkedOnPastaParticle.transform.position = this.transform.position;
+        }
 
+        /*for (int i = 0; i < _throughput; i++)
+        {
+            currentryWorkedOnPastaParticle = GetFromPastaInMachineQueue().GetComponent<PastaParticleControler>();
+            //currentryWorkedOnPastaParticle.transform.position = this.transform.position+new Vector3(-0.1f, 0, 0);
+        }
+
+        //currentryWorkedOnPastaParticle2.transform.position = this.transform.position + new Vector3(-0.1f, 0, 0); ;*/
         workTimer.Start();
     }
 
     private void SendToNextMachine(object source, ElapsedEventArgs e)
     {
         workTimer.Stop();
-        currentryWorkedOnPastaParticle1.movementToggle = true;
-        currentryWorkedOnPastaParticle2.movementToggle = true;
+        //currentryWorkedOnPastaParticle.movementToggle = true;
+        //currentryWorkedOnPastaParticle2.movementToggle = true;
+        for(int i=0;i<_throughput;i++)
+        {
+            currentryWorkedOnPastaParticle = GetFromPastaInMachineQueue().GetComponent<PastaParticleControler>();
+            UnityEngine.Debug.Log(currentryWorkedOnPastaParticle);
+            currentryWorkedOnPastaParticle.movementToggle = true;
+
+        }
         _isWorking = false;
     }
 
@@ -109,6 +129,35 @@ public class Machine : MonoBehaviour
         pastaQueue.Enqueue(pastaParticle);
         ArrangeQueue();
     }
+
+    private GameObject GetFromPastaInMachineQueue()
+    {
+        GameObject temp = pasta_in_machine_Queue.Dequeue();
+        ArrangeQueueInMachine();
+        return temp;
+    }
+
+    private void ArrangeQueueInMachine()
+    {
+        Vector2 machinePosition = this.transform.position;
+        machinePosition += machinequeueBegginingOffset;
+
+        Vector2 offset = new Vector2();
+
+        foreach (GameObject pastaParticle in pasta_in_machine_Queue)
+        {
+            pastaParticle.transform.position = machinePosition + offset;
+            offset += machinenextQueuedParticleOffset;
+        }
+
+    }
+
+    public void AddToPastaInMachineQueue(GameObject pastaParticle)
+    {
+        pasta_in_machine_Queue.Enqueue(pastaParticle);
+        ArrangeQueueInMachine();
+    }
+
     #endregion
 }
 
--- Assets/Scripts/PastaParticleControler.cs
+++ Assets/Scripts/PastaParticleControler.cs
@@ -40,5 +40,6 @@ public class PastaParticleControler : MonoBehaviour
             movementToggle = false;
         }
     }
+    
 
 }
