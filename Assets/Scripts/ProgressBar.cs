46a8850757526a5c282991f0250a2f6051918fad
Jacek Wójtowicz <jacekwojtowicz98@gmail.com>
2020-05-29 19:31:14 +0200

    Paki progressu znacznie popchniete - nie umiem tylko ogarnac tego, by tworzyly sie w poprawnym miejscu, cos sie dzieje tez z podpinaniem do nich wartosci

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public float maximum;
+    public long current;
+    public Image fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();           
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (long)current / (float)maximum;
+        fill.fillAmount = fillAmount;
+    }
+}
